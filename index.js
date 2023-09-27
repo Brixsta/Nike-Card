2f5ce87caa9f61eb828d938529508d36ca7bd7d4
Brixsta <Brixsta@gmail.com>
2023-09-27 08:49:39 -0600

    clean up code with functions

--- index.js
+++ index.js
@@ -8,26 +8,53 @@ const numbers = document.querySelector(".numbers");
 const circles = numbers.children;
 
 circle1.addEventListener("click", () => {
-  circle1.style.outline = "5px solid rgba(0,0,0,.25)";
-  circle2.style.outline = "none";
-  dollarBox.classList.add("blue");
-  topPart.classList.add("blue");
-  dollarBox.classList.remove("red");
-  topPart.classList.remove("red");
-  shoe1.style.animationName = "shoe1-appear";
-  shoe2.style.animationName = "shoe2-vanish";
+  applyOutlines("blue");
+  applyColors("blue");
+  applyAnimations("blue");
 });
 
 circle2.addEventListener("click", () => {
-  circle2.style.outline = "5px solid rgba(0,0,0,.25)";
-  circle1.style.outline = "none";
-  dollarBox.classList.add("red");
-  topPart.classList.add("red");
-  dollarBox.classList.remove("blue");
-  topPart.classList.remove("blue");
-  shoe1.style.animationName = "shoe1-vanish";
-  shoe2.style.animationName = "shoe2-appear";
+  applyOutlines("red");
+  applyColors("red");
+  applyAnimations("red");
 });
+
+function applyAnimations(color) {
+  if (color === "blue") {
+    shoe1.style.animationName = "shoe1-appear";
+    shoe2.style.animationName = "shoe2-vanish";
+  }
+  if (color === "red") {
+    shoe1.style.animationName = "shoe1-vanish";
+    shoe2.style.animationName = "shoe2-appear";
+  }
+}
+
+function applyColors(color) {
+  if (color === "blue") {
+    dollarBox.classList.add("blue");
+    topPart.classList.add("blue");
+    dollarBox.classList.remove("red");
+    topPart.classList.remove("red");
+  }
+  if (color === "red") {
+    dollarBox.classList.add("red");
+    topPart.classList.add("red");
+    dollarBox.classList.remove("blue");
+    topPart.classList.remove("blue");
+  }
+}
+
+function applyOutlines(color) {
+  if (color === "blue") {
+    circle1.style.outline = "5px solid rgba(0,0,0,.25)";
+    circle2.style.outline = "none";
+  }
+  if (color === "red") {
+    circle2.style.outline = "5px solid rgba(0,0,0,.25)";
+    circle1.style.outline = "none";
+  }
+}
 
 // APPLY CLICK EVENT LISTENER TO CIRCLES
 for (let i = 0; i < circles.length; i++) {
